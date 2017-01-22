23c9dad14601985e92e3c5bb23a10d484a45db02
mjaycub <themarkjacob@gmail.com>
2017-01-22 18:49:50 +0000

    Q learning implementation started BUT not finished. Broken code because Ben demanded it be committed.

--- agent.py
+++ agent.py
@@ -1,19 +1,93 @@
 import argparse
 import logging
 import sys
+import numpy as np
 
 import gym
 from gym import wrappers
 
 import gym_ple
 
+class State():
+    def __init__(self, i, j):
+        self.qValueFall = 0.0
+        self.qValueJump = 0.0
+        self.i = i
+        self.j = j
+    def getQValueFall():
+        return self.qValueFall
+
+    def getQValueJump():
+        return self.qValueJump
+
+    def setQValueFall(val):
+        self.qValueFall = val
+
+    def setQValueJump(val):
+        self.qValueJump = val
+
+
 # The world's simplest agent!
 class RandomAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
+        self.lastAction = 0
+        self.lastState = None
+        self.moves = []
+        self.qValues = None
+        self.r = {0: 1, 1: -1000} # Reward
+        self.lr = 0.7
+        self.discount = 1.0
 
     def act(self, observation, reward, done):
-        return self.action_space.sample()
+       # Player y at index 7
+       playerY = observation['player_y']
+
+       # Next pipe bottom y
+       nextPipeBottomY = observation['next_pipe_bottom_y']
+
+       # vertical difference between player and bottom pipe
+       vDiff = int(abs(playerY - nextPipeBottomY))
+
+       # horizontal distance from next pipe
+       hDiff = int(observation['next_pipe_dist_to_player'])
+
+       # Get current state
+       state = self.qValues[vDiff][hDiff]
+
+       # Store last action/state and current state
+       self.moves.append([self.lastAction, self.lastState, state])
+       self.lastState = state
+
+       if (state.qValueJump > state.qValueFall):
+        self.lastAction = 0
+        return 0
+       else:
+        self.lastAction = 1
+        return 1
+
+    def updateQValues(self):
+        #Update Q Values from stored history
+        previousMoves = list(reversed(self.moves))
+
+
+        # q learning
+        t = 1
+        for move in previousMoves:
+            action = move[0]
+            prevState = move[1]
+            state = move[2]
+
+        # TODO:
+        if t==1 or t==2:
+            if (action == 0):
+                qvalues[prevState.i][prevState.j].qValueJump
+            else:
+                qvalues[prevState.i][prevState.j].qValueFall
+
+            self.qvalues[prevState.i][prevState.j] = (1- self.lr) * (self.qvalues[prevState.i][prevState.j]) + (self.lr) * ( self.r[1] + (self.discount)*max(self.qvalues[state]) )
+
+
 
 if __name__ == '__main__':
     # You can set the level to logging.DEBUG or logging.WARN if you
@@ -21,7 +95,8 @@ if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    env = gym.make('FlappyBird-v0' if len(sys.argv)<2 else sys.argv[1])
+    
+    env = gym.make('FlappyBird-v0' if len(sys.argv)<2 else sys.arinit_arrygv[1])
 
     # You provide the directory to write to (can be an existing
     # directory, including one with existing data -- all monitor files
@@ -31,6 +106,8 @@ if __name__ == '__main__':
     env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
     agent = RandomAgent(env.action_space)
+    #agent.qValues = [[State() for j in range(500)] for i in range(500)]
+    agent.qValues = [[State(i,j) for j in range(500)] for i in range(500)]
 
     episode_count = 100
     max_steps = 200
@@ -39,11 +116,12 @@ if __name__ == '__main__':
 
     for i in range(episode_count):
         ob = env.reset()
-
+        # print(ob)
         for j in range(max_steps):
             env.render()
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
+            # print(env.action_space.__repr__())
             if done:
                 break
             # Note there's no env.render() here. But the environment still can open window and
